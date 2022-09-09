2a774146b2aa4e1c34b8841bb0cbff3d13c68bc0
MuraliKarre <muralidevops18@gmail.com>
2022-09-09 16:48:51 +0530

    Modified

--- Acronyms.py
+++ Acronyms.py
@@ -1,6 +1,6 @@
-user_input = str(input("Enter a Phrase: "))
-value = user_input.split()
+user_input = str(input("Enter a Phrase: ")).split()
+#value = user_input.split()
 a = " "
-for i in value:
+for i in user_input:
     a = a+str(i[0]).upper()
 print(a)
