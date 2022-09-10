40cecb75eeb0656ef112fc00d16be21dbda91c13
MuraliKarre <muralidevops18@gmail.com>
2022-09-10 18:26:44 +0530

    input values

---
+++ input.py
@@ -0,0 +1,13 @@
+while True:
+    reply = input("Enter Text: ")
+    if reply == 'Hello':
+        print("Hi Buddy...😍")
+    elif reply == 'Hi':
+        print("Hello Buddy...🤣")
+    elif reply == 'Bye':
+        print("See you soon...😂")
+    elif reply == 'Good':
+        print("Great...👌")
+    elif reply == 'stop':
+        print("BYE BYE...😄")
+        break
